aeed5259871cd7980b1d0e5f3f8de8b24df04188
zcielz <zciel_plus@outlook.com>
2019-03-26 00:50:38 +0800

    3.24

---
+++ pythoneBase/cn/zciel/selfstudy01/024.函数式编程_高阶函数MapAndReduce.py
@@ -0,0 +1,71 @@
+# map。map()函数接收两个参数，一个是函数，一个是Iterable，map将传入的函数依次作用到序列的每个元素，并把结果作为新的Iterator返回。
+def f(x):
+    return x * x
+
+
+r = map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(list(r))
+
+# map()传入的第一个参数是f，即函数对象本身。由于结果r是一个Iterator，Iterator是惰性序列，因此通过list()函数让它把整个序列都计算出来并返回一个list。
+# 如果不需要map()函数，写出一个循环，写一个循环，也可以计算出结果：
+L = []
+for n in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    L.append(f(n))
+print(L)
+
+# 把这个list所有数字转为字符串
+print(list(map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
+
+# 再看reduce的用法。reduce把一个函数作用在一个序列[x1, x2, x3, ...]上，这个函数必须接收两个参数，reduce把结果继续和序列的下一个元素做累积计算，其效果就是
+# reduce(f, [x1, x2, x3, x4]) = f(f(f(x1, x2), x3), x4)
+from functools import reduce
+
+
+def add(x, y):
+    return x + y
+
+
+print(reduce(add, [1, 3, 5, 7, 9]))
+
+
+# 当然求和运算可以直接用Python内建函数sum()，没必要动用reduce。
+# 但是如果要把序列[1, 3, 5, 7, 9]变换成整数13579，reduce就可以派上用场：
+def fn(x, y):
+    return x * 10 + y
+
+
+print(reduce(fn, [1, 3, 5, 7, 9]))
+
+
+# 考虑到字符串str也是一个序列，对上面的例子稍加改动，配合map()，我们就可以写出把str转换为int的函数：
+def fn(x, y):
+    return x * 10 + y
+
+
+def char2num(s):
+    digits = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+    return digits[s]
+
+
+print(reduce(fn, map(char2num, '13569')))
+# 整理成一个str2int的函数就是：
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
+
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+
+    def char2num(s):
+        return DIGITS[s]
+
+    return reduce(fn, map(char2num, s))
+
+
+# 还可以用lambda函数进一步简化成
+def char2num(s):
+    return DIGITS[s]
+
+
+def str2int(s):
+    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
